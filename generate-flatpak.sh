a01c25e2e04e9604b0ab10389febe25f09797615
Aaron Chang <xesuschang@gmail.com>
2019-05-06 17:17:48 +0800

    master: update script file to generate flatpak config

--- generate-flatpak.sh
+++ generate-flatpak.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-source $HOME/.rvm/scripts/rvm
+SUCCESS_CODE=0
+ERROR_CODE=1
+
 ruby bitmarkd.rb
+
+if [ "$?" != "$SUCCESS_CODE" ]; then
+    printf "error generate bitmarkd flatpak file"
+    exit "$ERROR_CODE"
+fi
+
 ruby recorderd.rb
+
+if [ "$?" != "$SUCCESS_CODE" ]; then
+    printf "error generate recorderd flatpak file"
+    exit "$ERROR_CODE"
+fi
+
+exit "$SUCCESS_CODE"
