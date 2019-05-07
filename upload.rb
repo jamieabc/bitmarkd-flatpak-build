16eb8a23e199fa1f1184638cf30cbe8e3e4ea154
Aaron Chang <xesuschang@gmail.com>
2019-05-07 15:14:44 +0800

    master: fix upload script

--- upload.rb
+++ upload.rb
@@ -13,15 +13,18 @@ bucket = 'bitmarkd-flatpak'
 
 s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
 obj = s3.bucket(bucket).object(file_name)
-file = File.open(file_name, 'rb')
 sha256 = Digest::SHA256.file file_name
 puts "file #{file_name} with sha-256: #{sha256}"
 
 # setup metadata
-obj.metadata['content-type'] = 'application/x-www-form-urlencoded; charset=utf-8'
-obj.metadata['x-amz-meta-sha256'] = sha256
-obj.metadata['x-amz-meta-date'] = Time.now.strftime("%Y-%d-%m")
-obj.metadata['x-amz-meta-version'] = ARGV[0]
+metadata = {
+  'content-type' => 'application/x-www-form-urlencoded; charset=utf-8',
+  'x-amz-meta-sha256' => sha256.to_s,
+  'x-amz-meta-date' => Time.now.strftime("%Y-%d-%m"),
+  'x-amz-meta-version' => ARGV[0]
+}
 
 puts "uploading file..."
-obj.upload_file(file)
+file = File.open(file_name, 'rb')
+obj.upload_file(file, metadata: metadata)
+file.close
