dfa19a68d1732251730b9ec454be7b2debf2bdd6
Aaron Chang <xesuschang@gmail.com>
2019-05-07 15:25:48 +0800

    master: fix content-type for s3 metadata

--- upload.rb
+++ upload.rb
@@ -10,21 +10,22 @@ end
 version = ARGV[0]
 file_name = ARGV[1]
 bucket = 'bitmarkd-flatpak'
+region = 'ap-northeast-1'
 
-s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
+s3 = Aws::S3::Resource.new(region: region)
 obj = s3.bucket(bucket).object(file_name)
 sha256 = Digest::SHA256.file file_name
 puts "file #{file_name} with sha-256: #{sha256}"
 
 # setup metadata
 metadata = {
-  'content-type' => 'application/x-www-form-urlencoded; charset=utf-8',
   'x-amz-meta-sha256' => sha256.to_s,
   'x-amz-meta-date' => Time.now.strftime("%Y-%d-%m"),
   'x-amz-meta-version' => ARGV[0]
 }
+content_type = 'application/x-www-form-urlencoded; charset=utf-8',
 
 puts "uploading file..."
 file = File.open(file_name, 'rb')
-obj.upload_file(file, metadata: metadata)
+obj.upload_file(file, metadata: metadata, content_type: content_type)
 file.close
