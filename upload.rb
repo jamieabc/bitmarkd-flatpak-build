64b8d1c2fd8d357bd01bd1dc0d590004f1f62eaf
Aaron Chang <xesuschang@gmail.com>
2019-05-07 12:17:40 +0800

    master: fix name

--- upload.rb
+++ upload.rb
@@ -9,9 +9,10 @@ end
 
 version = ARGV[0]
 file_name = ARGV[1]
+bucket = 'bitmarkd-flatpak'
 
 s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
-obj = s3.bucket('bitmarkd-flatpak').object('bitmarkd-test.flatpak')
+obj = s3.bucket(bucket).object(file_name)
 file = File.open(file_name, 'rb')
 sha256 = Digest::SHA256.file file_name
 puts "file #{file_name} with sha-256: #{sha256}"
