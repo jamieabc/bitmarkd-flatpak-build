948b95e02c390f7afd1e73cc043f2fc89cab3f8f
Aaron Chang <xesuschang@gmail.com>
2019-05-07 13:46:30 +0800

    master: add upload script

---
+++ upload.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+source ~/.rvm/scripts/rvm
+
+ERROR_CODE=1
+BITMARKD_FLATPAK_BUNDLE="bitmarkd.flatpak"
+RECORDERD_FLATPAK_BUNDLE="recorderd.flatpak"
+
+if [ $# -ne 1 ]; then
+    printf "Not enough argument\n"
+    exit "$ERROR_CODE"
+fi
+
+tag=$1
+
+ruby upload.rb "#{tag}" "$BITMARKD_FLATPAK_BUNDLE"
+ruby upload.rb "#{tag}" "RECORDERD_FLATPAK_BUNDLE"
