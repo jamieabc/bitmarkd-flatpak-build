b701354fde85aa3a5866c6993681f848f74daa19
Aaron Chang <xesuschang@gmail.com>
2019-05-08 14:25:10 +0800

    master: fix for recorderd package list

--- recorderd.rb
+++ recorderd.rb
@@ -45,10 +45,10 @@ class RecorderdBuild < BaseBuild
 
   def additional_module_shasum
     hsh = {}
-    %w(github.com/cihub/seelog
-        golang.org/x/crypto/ed25519
-        golang.org/x/crypto/sha3
-        golang.org/x/time/rate).each do |m|
+    [
+      'github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575',
+      'golang.org/x/crypto v0.0.0-20190506204251-e1dfcc566284'
+    ].each do |m|
       github_url = pkg_url(m)
       hsh[github_url] = url_file_shasum(github_url)
     end
