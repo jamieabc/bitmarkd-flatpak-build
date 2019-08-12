904326f2ddb16765263d2b61986eb1423656e0df
Aaron Chang <xesuschang@gmail.com>
2019-08-12 13:46:10 +0800

    fix typo

--- generate-flatpak.sh
+++ generate-flatpak.sh
@@ -10,7 +10,7 @@ help() {
     printf "\tgit_tag: github tag string, e.g. v0.10.6, master, etc.\n"
 }
 
-if [ $# -eq 0 ]; then
+if [[ $# -eq 0 ]]; then
     help
     exit "$ERROR_CODE"
 fi
@@ -21,7 +21,7 @@ git_tag=$1
 printf "generating bitmarkd flatpak file\n"
 ruby bitmarkd.rb "$git_tag"
 
-if [ "$?" != "$SUCCESS_CODE" ]; then
+if [[ "$?" != "$SUCCESS_CODE" ]]; then
     printf "error generate bitmarkd flatpak file\n"
     exit "$ERROR_CODE"
 fi
