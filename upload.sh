4dc7d3f052f3ab936f4c4701d6ea92b82b98f0ae
Aaron Chang <xesuschang@gmail.com>
2019-08-12 13:56:16 +0800

    remove recorderd upload

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ source ~/.rvm/scripts/rvm
 
 ERROR_CODE=1
 BITMARKD_FLATPAK_BUNDLE="bitmarkd.flatpak"
-RECORDERD_FLATPAK_BUNDLE="recorderd.flatpak"
+#RECORDERD_FLATPAK_BUNDLE="recorderd.flatpak"
 
 if [ $# -ne 1 ]; then
     printf "Not enough argument\n"
@@ -15,4 +15,4 @@ fi
 tag=$1
 
 ruby upload.rb "$tag" "$BITMARKD_FLATPAK_BUNDLE"
-ruby upload.rb "$tag" "$RECORDERD_FLATPAK_BUNDLE"
+#ruby upload.rb "$tag" "$RECORDERD_FLATPAK_BUNDLE"
